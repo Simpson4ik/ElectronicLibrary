0878cd2c2cccc2c8293d5d21e78fe7697d4c7d52
Oleksandr Voloshyn <volosins651@gmail.com>
2026-05-07 12:09:28 +0300

    add Book and Reader

--- ElectronicLibrary.Core/Entities/Reader.cs
+++ ElectronicLibrary.Core/Entities/Reader.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ElectronicLibrary.Core.Entities
+public class Reader
 {
-    internal class Reader
-    {
-    }
+    public int Id { get; set; }
+
+    // Refactoring Technique: Rename Variable.
+    public string FullName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
 }
