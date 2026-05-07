6c0e74a1b5bef6a616ead22417ac5023decfa129
Oleksandr Voloshyn <volosins651@gmail.com>
2026-05-07 11:08:07 +0300

    Створено проект та реалізовано перші налаштування

---
+++ ElectronicLibrary.Core/Constants/ValidationConstants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectronicLibrary.Core.Constants
+{
+    internal class ValidationConstants
+    {
+    }
+}
---
+++ ElectronicLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace ElectronicLibrary.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
