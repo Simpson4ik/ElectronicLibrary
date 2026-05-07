a207dd1ec849ede1ae8a545336f305450afb3ee3
Oleksandr Voloshyn <volosins651@gmail.com>
2026-05-07 12:36:40 +0300

    налаштував контекст бази даних та патерн роботи з даними

---
+++ ElectronicLibrary.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using ElectronicLibrary.Core.Entities;
+
+namespace ElectronicLibrary.Infrastructure.Data
+{
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Reader> Readers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
