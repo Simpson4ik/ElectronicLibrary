113dcf337210b050d3a142175726a34a6224c674
Oleksandr Voloshyn <volosins651@gmail.com>
2026-05-07 12:16:01 +0300

    Done

--- ElectronicLibrary.Core/Entities/Book.cs
+++ ElectronicLibrary.Core/Entities/Book.cs
@@ -2,9 +2,25 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ElectronicLibrary.Core.Entities
+namespace ElectronicLibrary.Core.Entities;
+/// <summary>
+/// Представляє сутність книги в системі електронної бібліотеки.
+/// </summary>
+// Principle: Single Responsibility (SOLID) - Клас відповідає виключно за структуру даних книги.
+public class Book
 {
-    internal class Book
-    {
-    }
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Author { get; set; } = string.Empty;
+
+    public string Isbn { get; set; } = string.Empty;
+
+    public int PublicationYear { get; set; }
+
+    public bool IsAvailable { get; set; } = true;
+
+    public DateTime AddedDate { get; set; } = DateTime.UtcNow;
 }
+
